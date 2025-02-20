2b050e21a1f45fd627a70da1f2aed835e7460241
hugogomes2 <Hl522052@gmail.com>
2025-02-19 21:57:16 -0300

    Criacao do CRUD de Usuários

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import {CreateUserDto} from './dto/create-user.dto';
+@Injectable()
+export class UsersService {}
